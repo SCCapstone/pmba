0bdb7b6c3311b7b6aafcfd25e4dc4892625ca389
Jeff Dumas <jdumas@email.sc.edu>
2015-11-12 18:44:13 -0500

    Merge branch 'master' of https://github.com/SCCapstone/pmba into jeffdd
    # Conflicts:
    #	router/route.js

--- route.js
+++ route.js
@@ -6,20 +6,20 @@ Router.route('/', {
 
 Router.route('/login');
 Router.route('/changePassword');
-//Router.route('/admin_overall');
+Router.route('/admin_overall');
 Router.route('/updateStudentInfo');
 Router.route('/admin_student');
 Router.route('/addStudent');
-//Router.route('/student');
+Router.route('/student');
 Router.route('/statistics');
 Router.route('/help');
 Router.route('/infoPage');
 
-
+/*
 Router.route('/student', function () {
     this.render('student');
 });
 
 Router.route('/admin_overall', function () {
     this.render('admin_overall');
-})
+})*/

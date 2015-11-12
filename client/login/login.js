34b2bf6b9068747ef9f07ef70c6ecd0b08a482d8
spearsk <kspears@email.sc.edu>
2015-11-11 22:13:16 -0500

    Fixed login, updateinfo, change password
    Wrong _id was being passed through so update info would NEVER have
    worked. Fixed now

--- client/login/login.js
+++ client/login/login.js
@@ -1,25 +1,27 @@
 
  if (Meteor.isServer) {
-  Meteor.publish("login", function () {
+  Meteor.publish("login","studentInfo", function () {
 
     return Users.find({});
   });
 }
 
 if (Meteor.isClient) {
-  Meteor.subscribe("login");
+  Meteor.subscribe("login","studentInfo");
 
 	Template.login.events({
 		'submit form' : function(event){
 			event.preventDefault();
 			var user = login.findOne({UserID: inputEmail.value});
+			var userInfo = studentInfo.findOne({UserID: inputEmail.value});
+			
 			if(user.UserID == inputEmail.value && user.Password == inputPassword.value && user.TempPassword=="Y"){
 				console.log("You are using a temp password, must reset");
-				window.location.href = "/changePassword" + "#" + inputEmail.value;
+				window.location.href = "/changePassword" + "#" + user._id;
 			}
 			else if(user.UserID == inputEmail.value && user.Password == inputPassword.value){
 				if(user.IDType=="S"){
-					window.location.href = "/student" + "#" + inputEmail.value;
+					window.location.href = "/student" + "#" + userInfo._id;
 				}
 				else{
 					window.location.href = "/admin_overall";

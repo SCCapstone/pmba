8f662063dc6e0913fc5ae3d594c924e66e22878d
smitht57 <smitht57@email.sc.edu>
2015-11-12 10:58:21 -0500

    added code for admin_overall viewing the list of forms

--- client/admin/admin_overall.js
+++ client/admin/admin_overall.js
@@ -16,3 +16,29 @@ if (Meteor.isClient) {
     });
 
 }
+
+if (Meteor.isServer) {
+    Meteor.publish("forms", function () {
+        var c = forms.find().count();
+        alert(c);
+        return forms.find({});
+    });
+
+}
+if (Meteor.isClient) {
+    Meteor.subscribe("forms");
+    Template.student.helpers({
+        forms: function () {
+            var c =forms.find({}).count();
+            alert(c);
+            return forms.find({});
+
+        }
+    });
+    Template.student.events({
+        'click .toggle-checked' : function(event){
+            event.preventDefault();
+
+        }
+    })
+}

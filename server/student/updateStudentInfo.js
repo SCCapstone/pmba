815695253fb440bcf609f4fd9ca5d872a0bbc9a0
spearsk <kspears@email.sc.edu>
2015-11-11 22:52:12 -0500

    Cleanup Time
    Cleaned up all as much code as i could without breaking the site.

--- server/student/updateStudentInfo.js
+++ server/student/updateStudentInfo.js
@@ -1,25 +1,2 @@
-Users = new Mongo.Collection('studentInfo');
- 
- if (Meteor.isServer) {
-  Meteor.publish("studentInfo", function () {
-    return Users.find({});
-  });
-}
-if (Meteor.isClient) {
-  Meteor.subscribe("studentInfo");
-	Template.updateStudentInfo.events({
-		'submit form' : function(event){
-			event.preventDefault();
-			
-			Users.insert({
-				UserID : inputEmail.value,
-				FirstName : firstName.value,
-				LastName : lastName.value,
-				Email : inputEmail.value,
-				CellNumber : cellNumber.value,
-				WorkNumber : workNumber.value,
-				HomeNumber : homeNumber.value,
-				Picture : "Upic1"});
-		}
-	});
-}
+users = new Mongo.Collection('studentInfo');
+

73ffd288717141bbf1bfaea09552c891bcdccd0b
smitht57 <smitht57@email.sc.edu>
2015-11-12 12:05:15 -0500

    Added addforms page. Doesn't currently work fully but looks like it

---
+++ client/admin/addforms.js
@@ -0,0 +1,26 @@
+if (Meteor.isServer) {
+    Meteor.publish("forms", function () {
+        return forms.find({});
+    });
+}
+if (Meteor.isClient) {
+    //Meteor.subscribe("login");
+    Meteor.subscribe("forms");
+    Template.addforms.events({
+        'submit form' : function(event){
+            //var userid = window.location.hash.substring(1);//selects current user email from url
+            //var user1 = login.findOne({UserID: userid});
+            //var user2 = studentInfo.findOne({UserID: userid});
+            //var id1 = user1._id;
+            //var id2 = user2._id;
+            //Users.update({_id: id2},{$set:{
+            //the code above needs to be fixed, but the insert command will not update, only creates a new entry
+            forms.insert({
+                Name : inputName.value,
+                Description : inputDescribe.value,
+                DueDate : duedate.value});
+            //login.update({_id: id1}, {$set:{UserID: inputEmail.value}});
+            alert("Your information has been updated!");
+        }
+    });
+}

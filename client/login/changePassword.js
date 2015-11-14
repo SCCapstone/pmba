5d3e0f0bad548148711a743145e56bcde43d2aa1
spearsk <kspears@email.sc.edu>
2015-11-14 11:13:52 -0500

    Added some comments in login
    commenting code is good mkay

--- client/login/changePassword.js
+++ client/login/changePassword.js
@@ -11,9 +11,9 @@ if (Meteor.isClient) {
 		'submit form' : function(event){
 			event.preventDefault();
 			var id = window.location.hash.substring(1);//selects current user email from url
-			var user = login.findOne({_id: id});
-			var userEmail = user.UserID;
-			var student = studentInfo.findOne({UserID: userEmail});
+			var user = login.findOne({_id: id});//look in login collection for id
+			var userEmail = user.UserID;//select email from login database corresponding with the userid previously
+			var student = studentInfo.findOne({UserID: userEmail});//select student from email we got above
 			
 			if(oldPassword.value == user.Password && newPassword.value !== user.Password && confirmNewPassword.value==newPassword.value){
 				login.update({_id: id},{$set:{Password: newPassword.value, TempPassword: "N"}});

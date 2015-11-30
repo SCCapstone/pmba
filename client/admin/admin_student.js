61fb3a760b6a40f4b534d8844d08518700bb3088
Jeff Dumas <jdumas@email.sc.edu>
2015-11-29 23:14:46 -0500

    Auto fills in student's email in 'admin_student'

--- client/admin/admin_student.js
+++ client/admin/admin_student.js
@@ -15,7 +15,7 @@
         /** This function returns an array containing the fields for the student stored in the session
          variable 'selectedStudent so that it can be iterated over and displayed'
          **/
-        data: function() {
+        profile: function() {
             return studentInfo.find(Session.get('selectedStudent')).fetch();
         }
 
--- client/sendEmail/sendEmail.js
+++ client/sendEmail/sendEmail.js
@@ -1,3 +1,5 @@
+Meteor.subscribe("studentInfo");
+
 Template.sendEmail.events({
     'click .btn-success' : function(event){
         event.preventDefault();
@@ -7,5 +9,12 @@ Template.sendEmail.events({
         var message = messageInput.value;
         console.log("address: " + address + "\tsubject: " + subject + "\tmessage: " + message);
         Meteor.call('sendEmail', address, subject, message);
+    }
+});
+
+Template.sendEmail.helpers({
+    profile: function() {
+        console.log(studentInfo.find(Session.get('selectedStudent')).fetch());
+        return studentInfo.findOne(Session.get('selectedStudent'));
     }
 });

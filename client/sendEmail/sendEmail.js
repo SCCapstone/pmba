9e734a591bbe198973432b2f9c2bd6459219e8f2
Jeff Dumas <jdumas@email.sc.edu>
2015-11-30 00:09:00 -0500

    Took out some debug code

--- client/sendEmail/sendEmail.js
+++ client/sendEmail/sendEmail.js
@@ -3,18 +3,15 @@ Meteor.subscribe("studentInfo");
 Template.sendEmail.events({
     'click .btn-success' : function(event){
         event.preventDefault();
-        console.log("click");
         var address = addressInput.value;
         var subject = subjectInput.value;
         var message = messageInput.value;
-        console.log("address: " + address + "\tsubject: " + subject + "\tmessage: " + message);
         Meteor.call('sendEmail', address, subject, message);
     }
 });
 
 Template.sendEmail.helpers({
     profile: function() {
-        console.log(studentInfo.find(Session.get('selectedStudent')).fetch());
         return studentInfo.findOne(Session.get('selectedStudent'));
     }
 });

8b4584ffb8ff0e08d2d6097d5ae7720f0077a26d
Jeff Dumas <jdumas@email.sc.edu>
2015-12-01 21:04:20 -0500

    Merge branch 'master' of https://github.com/SCCapstone/pmba into jeffdd
    # Conflicts:
    #	server/addForms/addForms.js

--- client/student/infoPage.js
+++ client/student/infoPage.js
@@ -1,9 +1,14 @@
-    Meteor.subscribe("forms");
-    Template.infoPage.helpers({
-        forms: function () {
-            return forms.find({_id : Session.get('selectedForm')});
-        },
-        selectedForm : function () {
-            return Session.get("selectedForm")
-        }
-    });
+Meteor.subscribe("forms");
+Template.infoPage.helpers({
+    forms: function () {
+        return forms.find({});
+    },
+    studentInfo: function() {
+        var temp = "ID";
+        temp = temp +2;
+        return studentInfo.find({UserID: temp});
+    },
+    profile: function() {
+        return forms.find(Session.get('selectedForm')).fetch();
+    }
+});

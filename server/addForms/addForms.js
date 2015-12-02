bfe66f11963b06c8085f06845269692c2d2bbf64
Jeff Dumas <jdumas@email.sc.edu>
2015-12-01 20:45:51 -0500

    Merge branch 'master' of https://github.com/SCCapstone/pmba into jeffdd
    # Conflicts:
    #	server/addForms/addForms.js

--- server/addForms/addForms.js
+++ server/addForms/addForms.js
@@ -3,27 +3,30 @@ Meteor.publish("forms", function () {
 });
 
 Meteor.methods({
-    addForm: function (name, description, dueDate) {
-       /* if (Meteor.userId() &&
+    addForm: function (name, description, dueDate, num, pic) {
+        /* if (Meteor.userId() &&
          studentInfo.findOne(Meteor.userId(), {fields: {'IDType': 1}}).IDType == 'A') {*/
         var formIsThere = forms.find({Name: name}, {limit: 1}).count() > 0;
         if (formIsThere) {
             console.log("A Form With That Name Already Exists");
-         }
+        }
         else {
             console.log("INSERTING FORM INFO");
             forms.insert({
                 Name: name,
                 Description: description,
-                DueDate: dueDate
+                DueDate: dueDate,
+                FormNumber: num,
+                FormPicture: pic
             });
             var $set = {};
             $set['Forms.' + name] = false;
             studentInfo.upsert({},
                 {$set: $set}, {multi: true});
+            //}
+            /* else {
+             console.log("Can't add form not logged in or not an admin");
+             }*/
         }
-     /*   else {
-            console.log("Can't add form not logged in or not an admin");
-        }*/
     }
 });

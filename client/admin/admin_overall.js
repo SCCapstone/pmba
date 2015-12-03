67f9ca02998ec4db3dbd65f391448b7bf51c7dd3
Jeff Dumas <jdumas@email.sc.edu>
2015-12-02 23:12:14 -0500

    percentage bars work but they are really small

--- client/admin/admin_overall.js
+++ client/admin/admin_overall.js
@@ -2,8 +2,6 @@
     Meteor.subscribe("studentInfo");
 	Meteor.subscribe("forms");
 
-    var test = 100;
-
     Template.adminOverall.helpers({
         studentInfo: function () {
             return studentInfo.find({});
@@ -11,19 +9,25 @@
         forms: function () {
             return forms.find({});
         },
+        // I'm not sure why this won't work
         percent: function (name) {
             Meteor.call('formPercent', name, function(error, result) {
                 if (error) {
                     console.log(error.reason);
                     return;
                 }
-                console.log('returning num: ' + parseInt(result));
-                return parseInt(result);
+                var num = parseInt(result);
+                return num;
             });
         },
-        test: function() {
-            var num = 100;
-            return num;
+        // This works but should probably executed on server
+        formPercent: function (name) {
+            var $qry = {};
+            $qry['Forms.' +name] = true;
+            var numCompleted = studentInfo.find($qry).count();
+            var total = studentInfo.find().count();
+            var percentage = numCompleted / total;
+            return percentage * 100;
         }
     });
 
--- server/admin/admin_overall.js
+++ server/admin/admin_overall.js
@@ -7,13 +7,11 @@ Meteor.publish("studentInfo", function () {
 
 Meteor.methods({
     formPercent: function (name) {
-        console.log('Form name = ' +name);
         var $qry = {};
         $qry['Forms.' +name] = true;
         var numCompleted = studentInfo.find($qry).count();
         var total = studentInfo.find().count();
         var percentage = numCompleted / total;
-        console.log('percentage = ' + percentage*100 +'%');
         return percentage * 100;
     }
 });

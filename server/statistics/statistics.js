65b1270703d4edc24f9b38b120771a9759096f31
spearsk <kspears@email.sc.edu>
2015-12-04 18:47:48 -0500

    Working on stats

--- server/statistics/statistics.js
+++ server/statistics/statistics.js
@@ -2,9 +2,7 @@
  * Created by userpc on 11/5/2015.
  */
 Meteor.methods({
-    getStats:function() {
-    },
-	getNumStudentsFormXCompleted: function(number){
+		getNumStudentsFormXCompleted: function(number){
 		var formName = "Forms.Name" + number;
 		var numStudents = studentInfo.find({IDType: { $in: ['S']}}).count();
 		console.log(numStudents + " total number of students.");

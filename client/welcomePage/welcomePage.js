50ed2e08a72f5c6766c1c783d7aa1d59509fb144
nandita <nandit95@yahoo.com>
2016-01-17 12:32:27 -0500

    Added 3 .js .html .css files for welcome page

---
+++ client/welcomePage/welcomePage.js
@@ -0,0 +1,25 @@
+Meteor.subscribe("forms");
+Meteor.subscribe("studentInfo");
+Meteor.subscribe("adminInfo");
+Template.welcomePage.helpers({
+    forms: function () {
+        return forms.find({});
+    },
+    profile: function() {
+        return forms.find(Session.get('selectedForm')).fetch();
+    },
+    AccountType: function () {
+        var student = studentInfo.findOne({_id: Meteor.userId()});
+        //var admin = adminInfo.findOne({_id: Meteor.userId()});
+        if (student.IDType === 'S') {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    },
+    studnets: function () {
+        return studentInfo.find({});
+    },
+});

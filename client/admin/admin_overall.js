f394f0026ce8e5a729f70209a0cd94bee6956ae1
zhtrim <trimz@email.sc.edu>
2016-02-04 09:12:37 -0500

    small changes and quick fixes

--- client/admin/admin_overall.js
+++ client/admin/admin_overall.js
@@ -29,18 +29,20 @@
             var deleteEmail;
             var deleteID = event.target.id;
             var account = studentInfo.findOne({_id:deleteID });
+            //if the selected account is a student then it will enter the if statement
+            //this if statement calls a function to remove the student account from the database
             if (account.IDType === 'S') {
                 holder = studentInfo.findOne({_id: deleteID});
                 deleteEmail = holder.Email;
                 Meteor.call('deleteAccount', deleteID, deleteEmail, true );
             }
+            //if the selected account is a admin then it will enter the else statement
+            //this else statement calls a function to remove the admin account from the database
             else {
                 holder = adminInfo.findOne({_id: deleteID});
                 deleteEmail = holder.Email;
                 Meteor.call('deleteAccount', deleteID, deleteEmail, false );
             }
             //Session.get("selectedStudent"); work in progress for a list of people to be deleted
-            //calls a function on the server side and sends the target id and email
-            //function removes the documents with the email or id in the collections
         }
     });
--- server/admin/admin_overall.js
+++ server/admin/admin_overall.js
@@ -6,9 +6,9 @@ Meteor.publish("studentInfo", function () {
 });
 
 Meteor.methods({
-    //need to add a check for student or admin account
-    //possible that it could be better to find out account type before and send as a variable
     deleteAccount: function(deleteID, deleteEmail, accountType) {
+        //if the account is a student it enters teh if statement
+        //other wise it will enter the else statement
         if (accountType) {
             studentInfo.remove(deleteID);
             FormStatus.remove({Email:deleteEmail});

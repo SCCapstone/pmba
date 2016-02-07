31d6b87d46a2f13ff4f4af100c3fcd67e61a1b4a
spearsk <kspears@email.sc.edu>
2016-02-07 11:28:04 -0500

    WORKING ON GRID

--- client/formsTable/formsTable.js
+++ client/formsTable/formsTable.js
@@ -1,6 +1,14 @@
 Meteor.subscribe("forms");
-Template.formsTable.helpers({
-    formsList: function () {
-        return forms;
+Template.fTable.helpers({
+    tableSettings : function () {
+      return {
+          fields: [
+            { key: 'Name', label: 'Form Name' },
+            { key: 'Description', label: 'Description' }
+          ]
+	}},
+	
+	formNames : function () {
+      return forms.find({}, {sort: {Name: 1}});
     }
 });

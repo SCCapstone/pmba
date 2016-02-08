bb34f93768b56ba457e4f384206ef1cc5c0eff7e
Jeff Dumas <jdumas@email.sc.edu>
2016-02-07 23:47:13 -0500

    forgot password more user friendly

--- server/resetPassword/resetPassword.js
+++ server/resetPassword/resetPassword.js
@@ -2,9 +2,13 @@ Meteor.publish("studentInfo", function () {
     return studentInfo.find();
 });
 
+// Sends reset password email to user if
+// the email address they provide is valid
 Meteor.methods({
     resetPass:function(email) {
-        var user = Meteor.users.findOne({'emails.address': email})._id;
-        Accounts.sendResetPasswordEmail(user, email);
+        if (Meteor.users.findOne({'emails.address': email})) {
+            var user = Meteor.users.findOne({'emails.address': email})._id;
+            Accounts.sendResetPasswordEmail(user, email);
+        }
     }
 });

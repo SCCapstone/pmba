9190de69f11217e7f64fb5e43a8bb424d24fad13
zhtrim <trimz@email.sc.edu>
2016-02-18 12:16:32 -0500

    fixing local

---
+++ client/resetPassword/resetPassword.js
@@ -0,0 +1,17 @@
+Meteor.subscribe("studentInfo");
+
+Template.resetPassword.events({
+    'submit form': function (event) {
+        event.preventDefault();
+        var emailVar = inputEmail.value;
+        console.log(emailVar);
+        Meteor.call('resetPass', emailVar);
+
+        sAlert.error('An email has been sent to reset your password if your account was found',
+            {
+                timeout: 1500,
+                offset: '120px',
+                position: 'bottom'
+            });
+    }
+});

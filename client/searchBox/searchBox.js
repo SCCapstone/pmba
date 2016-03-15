498ceaa55d2b375598b45bd9e09cd0e680fbe979
Jeff Dumas <jdumas@email.sc.edu>
2016-03-14 22:11:01 -0400

    Added student search for admin only work on first name right now

---
+++ client/searchBox/searchBox.js
@@ -0,0 +1,6 @@
+Template.searchBox.helpers({
+        inputAttributes: function () {
+            return {'class': 'easy-search-input', 'placeholder': 'Search...' };
+        },
+        studentIndex: () => StudentIndex
+});

ed5b375850ced7993867159c8672b97f72202168
Jeff Dumas <jdumas@email.sc.edu>
2016-04-01 20:28:16 -0400

    Added mass email in notifications page

--- client/admin/notifications/notifications.js
+++ client/admin/notifications/notifications.js
@@ -3,24 +3,35 @@ Meteor.subscribe("studentInfo");
 Meteor.subscribe("forms");
 
 Template.adminOverall.helpers({
-    studentInfo: function () {
+  /*  studentInfo: function () {
         return studentInfo.find({});
-    },
+    },*/
     forms: function () {
         return forms.find({});
-    },
-    // This works but should probably executed on server
-    formPercent: function (name) {
-        var numCompleted = FormStatus.find({$and: [{'FormName': name},{Done: true}]}).count();
-        var total = studentInfo.find().count();
-        var percentage = numCompleted / total;
-        return (percentage * 100).toFixed(0);
-
     }
 });
 
-Template.adminOverall.events({
-    'click .btn' : function(event){
+Template.notifications.events({
+    'click .btn-success' : function(event){
+        console.log("Button clicked");
+        //prevent defaults keeps the page from refreshing before we want it to
         event.preventDefault();
+        var subject = subjectInput.value;
+        var message = messageInput.value;
+        console.log("???");
+        var cursor = studentInfo.find();
+        cursor.forEach(function (student) {
+            console.log("test:");
+            console.log("sending email to " + student.FullName);
+            var address = student.Email;
+            Meteor.call('sendEmail', address, subject, message);
+
+            // Update SentEmails field for student
+            studentInfo.update(student.id, {
+                $set: {
+                    SentEmails: this.SentEmails += 1
+                }
+            });
+        });
     }
 });

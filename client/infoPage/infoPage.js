166f9efc0f61ee164fe956c43514fa3cb09dbd80
zhtrim <trimz@email.sc.edu>
2016-04-14 11:10:08 -0400

    Students how have not completed a form are now displayed on the info page for that form

--- client/infoPage/infoPage.js
+++ client/infoPage/infoPage.js
@@ -20,41 +20,10 @@ Template.infoPage.helpers({
     },
     students: function () {
 		var name = forms.findOne(Session.get('selectedForm')).Name;
-		var cursor = FormStatus.find({FormName: name});
-		cursor.forEach(function(doc){
-			var email = doc.Email;
-			var studentName=studentInfo.findOne({Email: email}).FullName;
-			var formID = FormStatus.findOne({Email: email, FormName: name})._id;
-			alert(studentName);
-			FormStatus.update({_id: formId}, {$set: {fullName:studentName}});
-			alert("here");
-		});
-		
-		/**var cursor = FormStatus.find({FormName: name, Done: false});
-		cursor.forEach(function (doc) {
-			var email = doc.Email;
-			var studentID = studentInfo.findOne({Email: email})._id;
-			Session.set('incompletedStudents', studentID);
-			alert(email);
-		});
-		/**var selected = document.getElementById("SelectedStudents").value;
-		if(selected === "completed"){
-			alert("done");
-		}
-		else if(selected === "incompleted"){
-			alert("not");
-		}
-		else{
-			alert("all");
-		}
-		//var cursor = formstatus.find({FormName: name});
-		/**
-		set up a if ifelse else statement with the ocnditions
-			if : selected all, return all students found.
-			if else : selected done, return done students
-			else : selected not done, return not done students.
-		**/
-		
-        return studentInfo.find(Session.get('incompletedStudents')).fetch();
-    }
+		return FormStatus.find({FormName: name, Done:false});
+    },
+	studentId: function(email) {
+		var returnID = studentInfo.findOne({Email: email})._id;
+		return returnID;
+	}
 });

34084ad125c851a286f5625b9e408e070f7af885
Jeff Dumas <jdumas@email.sc.edu>
2016-04-20 09:58:44 -0400

    Fixed sending duplicate emails

--- client/sendEmail/sendEmail.js
+++ client/sendEmail/sendEmail.js
@@ -1,12 +1,13 @@
 Meteor.subscribe("studentInfo");
 
 Template.sendEmail.events({
-    'click .btn-success' : function(event){
+    'click #sendButton' : function(event){
 		//prevent defaults keeps the page from refreshing before we want it to
         event.preventDefault();
         var address = addressInput.value;
         var subject = subjectInput.value;
         var message = messageInput.value;
+        console.log('SENDING EMAIL');
         Meteor.call('sendEmail', address, subject, message);
 
         // Update SentEmails field for student

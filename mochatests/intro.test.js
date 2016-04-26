726b5056368a88754c78279e492c84ed7567c88e
Nandita K <nandit95@yahoo.com>
2016-04-26 15:04:07 -0400

    added some basic login/info page unit tests

--- mochatests/intro.test.js
+++ mochatests/intro.test.js
@@ -1,4 +1,7 @@
 /**
+ * Unit Testing
+ *
+ * Notes:
  * Before testing, have mocha and chai installed with
  * the following commands:
  *         meteor add practicalmeteor:mocha
@@ -20,10 +23,13 @@
  * the passes, the failures, the duration of the tests,
  * and a check mark next to the description of the tests
  * if they have succeeded.
+ *
  */
 
 var chai = require('chai');
+var chaiHttp = require('chai-http'); //required to run tests in the browser
 var assert = chai.assert;
+chai.use(chaiHttp);
 
 describe('Array', function() {
     it('should start empty', function() {
@@ -33,11 +39,56 @@ describe('Array', function() {
     });
 });
 
+describe('Array Element', function(){
+    it('should loop through the entire array', function() {
 
-/*describe('Page setTitle', function () {
-    it('should be set to PMBA @watch', function () {
-        browser.url('localhost:3000/login');
-        expect(browser.getTitle()).to.equal('PMBA');
+        var i = 0;
+
+        [1,2,3].forEach(function() {
+            i++;
+        });
+
+        assert.equal(i, 3);
+
+    })
+});
+
+
+
+describe('Homepage', function() {
+    it('should open homepage', function() {
+        chai.request('http://localhost:3000')
+            .put('/login')
+    })
+})
+
+describe('App Title', function () {
+    it('should be set to PMBA', function () {
+
+        chai.request('http://localhost:3000')
+            .put('/login')
+        
+        assert.equal('PMBA', 'PMBA', 'title is equal')
     });
 });
-*/
+
+describe('Login', function () {
+    it('should login', function() {
+
+        chai.request('http://localhost:3000')
+
+            .put('/login')
+            .send({ email: 'student4@student.com', password: 'password' })
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                expect(res).to.have.property(email);
+                expect(res).to.have.property(password);
+            });
+
+    });
+
+});
+
+
+
+
